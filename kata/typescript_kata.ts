6ba0ca34a4f185c516679e3b85df34e22d6aeb1d
mattluffman <45374018+mattluffman@users.noreply.github.com>
2020-01-02 15:07:49 -0500

    add sumArray()
    https://www.codewars.com/kata/576b93db1129fcf2200001e6/train/typescript

--- kata/typescript_kata.ts
+++ kata/typescript_kata.ts
@@ -20,10 +20,10 @@ console.log(boolToWord(false));
  *
  * sources:
  */
-export function isIsogram(str: string){
+export function isIsogram(str: string) : boolean {
     const splitChars = str.toLowerCase().split("");
     const unique = new Set<string>(splitChars);
-    console.log("size => " + unique.size);
+    console.log(`size => ${unique.size}`);
 
     return unique.size == str.length;
 }
@@ -47,6 +47,53 @@ describe("example", function() {
     assert.equal( isIsogram("moOse"), false, "same chars may not be same case" );
     assert.equal( isIsogram("isIsogram"), false );
     assert.equal( isIsogram(""), true, "an empty string is a valid isogram" );
+  });
+});
+ */
+
+
+/**
+ * started: 1/2/20
+ * kata: https://www.codewars.com/kata/576b93db1129fcf2200001e6/train/typescript
+ * solutions: https://www.codewars.com/kata/576b93db1129fcf2200001e6/solutions/typescript
+ * topics: min, max, spread syntax, reduce, string template literals
+ *
+ * sources: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Spread_syntax
+ * https://codeburst.io/javascript-arrays-finding-the-minimum-maximum-sum-average-values-f02f1b0ce332
+ *
+ */
+export function sumArray(array:number[]) : number {
+    //check for empty
+    if (array == null || array.length == 0 || array.length == 1) {
+        return 0;
+    }
+
+    const min = Math.min(...array);
+    const max = Math.max(...array);
+    console.log(`Min: ${min} Max: ${max}`);
+
+    //get sum including min & max
+    const totalSum = array.reduce((accumulator, currentVal) => {
+        return accumulator + currentVal
+    }, 0); //start with 0
+    console.log(`sum: ${totalSum}`);
+
+    //return original sum minus min & max. Then you don't have to worry about duplicates & such
+    return totalSum - max - min;
+}
+console.log(sumArray([ 6, 2, 1, 8, 10 ])); //16
+console.log(sumArray([ 6, 0, 1, 10, 10 ])); //17
+/*
+/// <reference path="/runner/typings/main/ambient/mocha/index.d.ts"/>
+/// <reference path="/runner/typings/main/ambient/chai/index.d.ts"/>
+import solution = require('./solution');
+
+import {assert} from "chai";
+
+describe("KataTest", function(){
+  it("BasicTests", function(){
+    assert.deepEqual(solution.sumArray([ 6, 2, 1, 8, 10 ]), 16);
+    assert.deepEqual(solution.sumArray([ 6, 0, 1, 10, 10 ]), 17);
   });
 });
  */
