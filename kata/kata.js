5920504d33403b1a79d8f5f8eb7c0b2acc6269ce
Matt Luffman <matt.luffman@gmail.com>
2026-05-25 12:08:47 -0400

    Refactor removeSmallest function: removed inline tests and added function export. Cleaned up code by eliminating commented-out test cases.

--- kata/kata.js
+++ kata/kata.js
@@ -197,38 +197,6 @@ function removeSmallest(numbers) {
     clone.splice(index, 1);
     return clone;
 }
-console.log(removeSmallest([1,2,3,4,5])); // = [2,3,4,5]
-console.log(removeSmallest([5,3,2,1,4])); // = [5,3,2,4]
-console.log(removeSmallest([2,2,1,2,1]));// = [2,2,2,1]
-/*
-Test.describe("removeSmallest", function() {
-  Test.it("works for the examples", function() {
-    Test.assertSimilar(removeSmallest([1, 2, 3, 4, 5]), [2, 3, 4, 5], "Wrong result for [1, 2, 3, 4, 5]");
-    Test.assertSimilar(removeSmallest([5, 3, 2, 1, 4]), [5, 3, 2, 4], "Wrong result for [5, 3, 2, 1, 4]");
-    Test.assertSimilar(removeSmallest([2, 2, 1, 2, 1]), [2, 2, 2, 1], "Wrong result for [2, 2, 1, 2, 1]");
-    Test.assertSimilar(removeSmallest([]), [], "Wrong result for []");
-  });
-
-  Test.it("returns [] if the list has only one element", function() {
-    for (let i = 0; i < 10; ++i) {
-      let x = ~~(Math.random() * 400);
-      Test.assertSimilar(removeSmallest([x]), [], `Wrong result for ${[x]}`);
-    }
-  });
-
-  function randomArray(length) {
-    return Array.from({length: length}, () => ~~(Math.random() * 400));
-  }
-
-  Test.it("returns a list that misses only one element", function() {
-    for(let i = 0; i < 10; ++i) {
-      let arr = randomArray(~~(Math.random() * 10) + 1);
-      let l = arr.length;
-      Test.assertSimilar(removeSmallest(arr).length, l - 1, `Wrong result for ${arr}`);
-    }
-  });
-});
- */
 
 /**
  * started 5/23/26
@@ -256,5 +224,6 @@ function multiplesOf3Or5(number) {
 }
 
 module.exports = {
+    removeSmallest,
     multiplesOf3Or5
 };
